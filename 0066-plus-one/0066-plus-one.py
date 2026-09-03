398b33119921fe794c6e1b129cf4a887564ba42b
dashsnehansh <dashsnehansh@gmail.com>
2026-09-03 18:02:09 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.95%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # Traverse the array from right to left
+        for i in xrange(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            
+        # If all digits were 9 (e.g., [9, 9] ->)
+        return [1] + digits
